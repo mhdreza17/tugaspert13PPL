7481fd2efa3acc7640eecbc873171e39c66bcccb
Muhammad Reza <mreza2795@gmail.com>
2026-01-13 17:00:03 +0700

    add selenium test for AUT new lagi

--- wait.sh
+++ wait.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Waiting for Selenium Hub..."
 for i in $(seq 1 30); do
-  if curl -s http://localhost:4444/wd/hub/status | grep -q "ready"; then
+  if curl -s http://localhost:4444/status | grep -q '"ready":[ ]*true'; then
     echo "Selenium Hub is ready!"
     exit 0
   fi
