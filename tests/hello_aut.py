a73b1a3a909acba41238f76cc5fc82aadf15ada3
Muhammad Reza <mreza2795@gmail.com>
2026-01-13 20:08:02 +0700

    paralel test

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -1,35 +1,35 @@
 import unittest
 import sys
-import time
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
 
     def setUp(self):
-        options = webdriver.FirefoxOptions()
+        browser = os.getenv("BROWSER", "firefox")
+
+        if browser == "chrome":
+            options = webdriver.ChromeOptions()
+        elif browser == "edge":
+            options = webdriver.EdgeOptions()
+        else:
+            options = webdriver.FirefoxOptions()
+
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
-        server = "http://localhost:4444"
 
+        server = "http://localhost:4444"
         self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
-        if len(sys.argv) > 1:
-            url = sys.argv[1]
-        else:
-            url = "http://docker-apache"
-
-        # Ensure proper URL format
-        if not url.startswith('http'):
-            url = "http://" + url
-
+        url = sys.argv[1] if len(sys.argv) > 1 else "http://localhost"
         self.browser.get(url)
-        self.browser.save_screenshot("screenshot.png")
-        expected_result = "Welcome back, Guest!"
-        actual_result = self.browser.find_element(By.TAG_NAME, 'p').text
-        self.assertIn(expected_result, actual_result)
+
+        expected = "Welcome back, Guest!"
+        actual = self.browser.find_element(By.TAG_NAME, 'p').text
+        self.assertIn(expected, actual)
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
